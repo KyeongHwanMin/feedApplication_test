dffa46788e7e6aac0150cbc6360456ee154bb857
KyeongHwan Min <h456542@naver.com>
2023-10-26 00:31:41 +0900

    FEAT : 회원가입 및 인증번호 확인 후 회원가입 승인

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from accounts.views import UserAPIView
+from accounts.views import UserAPIView, UserVerificationView
 
 urlpatterns = [
-    path("", UserAPIView.as_view(), name="userView"),
+    path("", UserAPIView.as_view(), name="user"),
+    path("verification", UserVerificationView.as_view(), name="user-verification"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,9 +5,38 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from accounts.models import User
 from accounts.serializers import UserSerializer
 
 
 class UserAPIView(APIView):
     def post(self, request):
-        pass
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserVerificationView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        verification_code = request.data.get("verification_code")
+
+        try:
+            user = User.objects.get(username=username)
+
+            if not user.check_password(password):
+                return Response("비밀번호가 일치하지 않습니다.", status=status.HTTP_400_BAD_REQUEST)
+
+            if user.verification_code != verification_code:
+                return Response("인증번호가 일치하지 않습니다.", status=status.HTTP_400_BAD_REQUEST)
+
+            user.is_active = True
+            user.save()
+
+            return Response("회원가입이 승인되었습니다.", status=status.HTTP_200_OK)
+
+        except User.DoesNotExist:
+            return Response("없는 계정입니다.", status=status.HTTP_400_BAD_REQUEST)
