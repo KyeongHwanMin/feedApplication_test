67df9b9c3d4d4c2957f527316cd05e4a0fb062e6
KyeongHwan Min <h45652@naver.com>
2023-10-26 22:26:35 +0900

    FEAT : post 모델 생성 및 마이그레이션

--- post/models.py
+++ post/models.py
@@ -1,1 +1,26 @@
+import uuid
 from django.db import models
+
+SOCIAL_CHOICE = (
+    ("facebook", "facebook"),
+    ("twitter", "twitter"),
+    ("instagram", "instagram"),
+    ("threads", "threads"),
+)
+
+
+class post(models.Model):
+    content_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, verbose_name="id"
+    )
+    type = models.CharField(
+        max_length=15, choices=SOCIAL_CHOICE, blank=True, verbose_name="유형"
+    )
+    title = models.CharField(max_length=128, verbose_name="제목")
+    content = models.TextField(verbose_name="내용")
+    hashtags = models.CharField(max_length=15, verbose_name="해시태그")
+    view_count = models.PositiveIntegerField(verbose_name="조회 수")
+    like_count = models.PositiveIntegerField(verbose_name="좋아요 수")
+    share_count = models.PositiveIntegerField(verbose_name="공유 수")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정 날짜")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성 날짜")
