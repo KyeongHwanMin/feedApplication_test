87dbe04f3dd8a0a5ffe938840ac1a4afa6c4b6cd
KyeongHwan Min <h45652@naver.com>
2023-10-26 21:59:07 +0900

    FEAT : post 앱 생성 및 연결

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/user/", include("accounts.urls")),
+    path("api/v1/post/", include("post.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
